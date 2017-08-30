f7b85bfbdd9d47ddefa8f24df3a4375012712e63
Ryan Loera <rloera@csumb.edu>
2017-08-29 21:43:40 -0400

    installed Alamofire and AlamofireImage cocoapods for this app

--- Pixel City/Controller/MapVC.swift
+++ Pixel City/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  Pixel City
 //
 //  Created by RyLo on 8/29/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
