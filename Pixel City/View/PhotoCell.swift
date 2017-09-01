669150295eaa84943f0573b439a44ed3b35ee7b0
Ryan Loera <rloera@csumb.edu>
2017-09-01 18:44:24 -0400

    added basic functions for collectionView and a label to let the user know how many photos are being downloaded

---
+++ Pixel City/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  Pixel City
+//
+//  Created by RyLo on 9/1/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
