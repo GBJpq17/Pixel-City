c355fdc2411052b91a3ddaf04fc1372dc3ef72e5
Ryan Loera <rloera@csumb.edu>
2017-09-02 08:42:59 -0400

    added function to get images  from Flickr API via JSON

--- Pixel City/Controller/MapVC.swift
+++ Pixel City/Controller/MapVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Alamofire
+import AlamofireImage
 
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
     
@@ -26,6 +28,8 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     
     var flowLayout = UICollectionViewFlowLayout()
     var collectionView: UICollectionView?
+    
+    var imageURLArray = [String]()
     
     
     override func viewDidLoad() {
@@ -140,15 +144,30 @@ extension MapVC: MKMapViewDelegate {
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
         mapView.addAnnotation(annotation)
         
-        print(FLICKR_URL(forAPIKey: API_KEY, withAnnonation: annotation, numberOfPhotos: 40))
-        
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+        retriveURLS(forAnnotation: annotation) { (true) in
+            print(self.imageURLArray)
+        }
     }
     
     func removePin() {
         for annotation in mapView.annotations {
             mapView.removeAnnotation(annotation)
+        }
+    }
+    
+    func retriveURLS(forAnnotation annotation: DroppablePin, handler: @escaping (_ status: Bool) -> ()) {
+        imageURLArray = []
+        Alamofire.request(FLICKR_URL(forAPIKey: API_KEY, withAnnonation: annotation, numberOfPhotos: 40)).responseJSON { (response) in
+            guard let json = response.result.value as? Dictionary<String, AnyObject> else {return}
+            let photosDict = json["photos"] as! Dictionary<String, AnyObject>
+            let photosDictArray = photosDict["photo"] as! [Dictionary<String, AnyObject>]
+            for photo in photosDictArray {
+                let postURL = "https://farm\(photo["farm"]!).staticflickr.com/\(photo["server"]!)/\(photo["id"]!)_\(photo["secret"]!)_h_d.jpg"
+                self.imageURLArray.append(postURL)
+            }
+            handler(true)
         }
     }
 }
--- Pixel City/Utilities/Constants.swift
+++ Pixel City/Utilities/Constants.swift
@@ -11,7 +11,5 @@ import Foundation
 let API_KEY = "a693835a38e58e6afdbcdc9e099384af"
 
 func FLICKR_URL(forAPIKey key: String, withAnnonation annotation: DroppablePin, numberOfPhotos number: Int ) -> String {
-    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
-    print(url)
-    return url
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
 }
